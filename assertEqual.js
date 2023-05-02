714a0f2a750a531723af93a14d1c3f959f025bbc
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-01 19:40:39 -0700

    Initial commit
     Changes to be committed:
    	new file:   .vscode/extensions.json
    	new file:   assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let result = (actual === expected) ? "✅✅✅ " + actual + " === " + expected : "❌❌❌ " + actual + " !== " + expected;
+  console.log(result);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
+assertEqual(2, 1);
+
