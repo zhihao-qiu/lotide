cd25bedcd945fbe92a1ebef513b5e87ce3f5f06f
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-03 15:16:12 -0700

    countLetters.js initiated

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let result = (actual === expected) ? `✅✅✅ Passed: ` : `❌❌❌ Failed: `;
+  console.log(result);
+  console.log(`   Expected: ${expected}   Actual Result: ${actual}`);
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countLetters = function(lettersToCount) {
+  const results = {};
+  const newLettersToCount = lettersToCount.replace(/ /g,'');
+
+  for (let letter of newLettersToCount) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+
+  return results;
+};
+
+// TEST CODE
+const processResult = countLetters('lighthouse in the house');
+
+assertEqual(processResult["l"], 1);
+assertEqual(processResult["i"], 2);
+assertEqual(processResult["g"], 1);
+assertEqual(processResult["h"], 4);
+assertEqual(processResult["t"], 2);
+assertEqual(processResult["o"], 2);
+assertEqual(processResult["u"], 2);
+assertEqual(processResult["s"], 2);
+assertEqual(processResult["e"], 3);
+assertEqual(processResult["n"], 1);
+assertEqual(processResult[" "], undefined);
