03aba84e92604dea1ee3216b7350fa530beeb9a1
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-02 18:15:56 -0700

    eqArrays.js initiated

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+// Assert Function to check if the function is working properly
+const assertEqual = function(actual, expected) {
+  let result = (actual === expected) ? `✅✅✅ ${actual} === ${expected}` : `❌❌❌ ${actual} !== ${expected}`;
+  console.log(result);
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  // compare the length of two arrays, return false if it's consistent
+  if (firstArray.length !== secondArray.length) {
+    return (false);
+  }
+
+  // loop to compare each elements in these two arrays
+  for (let i = 0; i < firstArray.length; i++) {
+    // compare the value of each element
+    if (firstArray[i] !== secondArray[i]) {
+      return (false);
+    }
+  }
+
+  return (true);
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // => should PASS
