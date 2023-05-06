3bcb3d591389d6e5fefb6aa040b61173a680f42c
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-05 21:19:17 -0700

    Recursive eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -6,11 +6,8 @@ const assertEqual = function(actual, expected) {
 
 // Assert Function to check if the function is working properly
 const assertArraysEqual = function(actual, expected) {
-  let result = (eqArrays(actual,expected)) ? `✅✅✅ Passed` : `❌❌❌ Failed`;
-  console.log(result);
-  console.log(' Expected: ');
-  console.log(expected);
-  console.log(' Actual Result: ');
+  const inspect = require('util').inspect; // <= add this line
+  let result = (eqObjects(actual, expected)) ? `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}` : `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
   console.log(actual);
 };
 
@@ -35,11 +32,18 @@ const eqArrays = function(firstArray, secondArray) {
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   // compare with the length
-  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-
+  if ((Object.keys(object1).length) !== (Object.keys(object2).length)) return false;
+  
   // compare with values
   let keys = Object.keys(object1);
   for (let key of keys) {
+    if ((object1[key].constructor === Object) && (object2[key].constructor === Object)) {
+      if (!eqObjects(object1[key], object2[key])) return false;
+    } else if ((object1[key].constructor !== Object) && (object2[key].constructor !== Object)) {
+      if (object1[key] !== object2[key]) return false;
+    } else { return false;}
+
+    /**
     // check Primitives of values
     if (!Array.isArray(object1[key])) {
       // when it's not an array, but a value
@@ -48,28 +52,33 @@ const eqObjects = function(object1, object2) {
       // when it's an array
       return eqArrays(object1[key], object2[key]);
     }
-    
+     */
+
   }
   return true;
 };
 
 // TEST CODE
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
+// const shirtObject = { color: "red", size: "medium" };
+// const anotherShirtObject = { size: "medium", color: "red" };
 // eqObjects(shirtObject , anotherShirtObject); // => true
-assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+// assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
 
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+// const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 // eqObjects(shirtObject , longSleeveShirtObject); // => false
-assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+// assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
 
 
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+// const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+// const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 // eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
-assertArraysEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true);
+// assertArraysEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true);
 
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+// const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 // eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
-assertArraysEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
+// assertArraysEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
+
+eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }); // => true
+//eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) // => false
+assertArraysEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
