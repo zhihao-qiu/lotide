a24bf69084a767d2369a1ed0c84b3abada884051
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-05-07 20:03:13 -0700

    refactor middle.js

--- middle.js
+++ middle.js
@@ -1,30 +1,4 @@
-// Assert Function to check if the function is working properly
-const assertArraysEqual = function(actual, expected) {
-  let result = (eqArrays(actual,expected)) ? `✅✅✅ Passed` : `❌❌❌ Failed`;
-  console.log(result);
-  console.log(' Expected: ');
-  console.log(expected);
-  console.log(' Actual Result: ');
-  console.log(actual);
-};
-
-const eqArrays = function(firstArray, secondArray) {
-  // compare the length of two arrays, return false if it's consistent
-  if (firstArray.length !== secondArray.length) {
-    return (false);
-  }
-
-  // loop to compare each elements in these two arrays
-  for (let i = 0; i < firstArray.length; i++) {
-    // compare the value of each element
-    if (firstArray[i] !== secondArray[i]) {
-      return (false);
-    }
-  }
-
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
 
 /** Function Name:  middle.js
   * Description:    take in an array and return the middle-most element(s) of the given array
@@ -48,12 +22,5 @@ const middle = function(array) {
   return rtnArray;
 };
 
+module.exports = middle;
 
-
-// TEST DATA
-assertArraysEqual(middle([1]), []); // => true
-assertArraysEqual(middle([1, 2]), []); // => true
-assertArraysEqual(middle([1, 2, 3]), [2]); // => true
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => true
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => true
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => true
